c3248d2c389eaa5a32d976f59cccdea65493d99e
DmitriyWaldaiskiy <d96765@yandex.ru>
2023-04-30 16:12:46 +0700

    сделаны закрытия через оверлей

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,38 @@ const imageCaption = document.querySelector(".popup__caption-image");
 
 // Открытие и закрытие попапов
 const openPopup = (popup) => {
+  document.addEventListener("keydown", handleEsc);
   popup.classList.add("popup_opened");
 };
 
 const closePopup = (popup) => {
+  document.removeEventListener("keydown", handleEsc);
   popup.classList.remove("popup_opened");
 };
+
+//закрытие по Esc
+function handleEsc(evt) {
+  if (evt.key === "Escape") {
+    const openPopup = document.querySelector(".popup_opened");
+    closePopup(openPopup);
+  }
+}
+
+//закрытие по оверлею
+function handleOverlay(evt, popup) {
+  if (evt.target.classList.contains("popup_opened")) {
+    closePopup(popup);
+  }
+}
+profilePopup.addEventListener("mousedown", (evt) =>
+  handleOverlay(evt, profilePopup)
+);
+addCardsPopup.addEventListener("mousedown", (evt) =>
+  handleOverlay(evt, addCardsPopup)
+);
+imagePopupOpen.addEventListener("mousedown", (evt) =>
+  handleOverlay(evt, imagePopupOpen)
+);
 
 //закрытие по крестику общее
 closeButtons.forEach((button) => {
@@ -61,34 +87,6 @@ const openButtonAddPopup = () => {
   openPopup(addPopupOpen);
 };
 addButton.addEventListener("click", openButtonAddPopup);
-
-// Добавление клона карточки
-const initialCards = [
-  {
-    name: "Звездное небо",
-    link: "https://images.unsplash.com/photo-1680742376777-3f66cc33513b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
-  },
-  {
-    name: "Дорога в лесу",
-    link: "https://images.unsplash.com/photo-1680158239346-8d2eb947ac52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
-  },
-  {
-    name: "Елка",
-    link: "https://images.unsplash.com/photo-1679775912575-cfe09ff563b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80",
-  },
-  {
-    name: "Лес",
-    link: "https://images.unsplash.com/photo-1677485449555-5df4c23d6ea3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=369&q=80",
-  },
-  {
-    name: "Шишка",
-    link: "https://images.unsplash.com/photo-1659891169235-9fc4ed6d0f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
-  },
-  {
-    name: "Олень",
-    link: "https://images.unsplash.com/photo-1676579533885-61b1d0d8a094?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=394&q=80",
-  },
-];
 
 const createCardsElement = (cardsData) => {
   const cardsElement = cardsTemplate.content
---
+++ scripts/initialcards.js
@@ -0,0 +1,27 @@
+// Добавление клона карточки
+const initialCards = [
+  {
+    name: "Звездное небо",
+    link: "https://images.unsplash.com/photo-1680742376777-3f66cc33513b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80",
+  },
+  {
+    name: "Дорога в лесу",
+    link: "https://images.unsplash.com/photo-1680158239346-8d2eb947ac52?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=327&q=80",
+  },
+  {
+    name: "Елка",
+    link: "https://images.unsplash.com/photo-1679775912575-cfe09ff563b8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80",
+  },
+  {
+    name: "Лес",
+    link: "https://images.unsplash.com/photo-1677485449555-5df4c23d6ea3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=369&q=80",
+  },
+  {
+    name: "Шишка",
+    link: "https://images.unsplash.com/photo-1659891169235-9fc4ed6d0f9a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "Олень",
+    link: "https://images.unsplash.com/photo-1676579533885-61b1d0d8a094?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=394&q=80",
+  },
+];
